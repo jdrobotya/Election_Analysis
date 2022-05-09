96718c7196bf76af1f97502600747414f423d985
jdrobotya <103322251+jdrobotya@users.noreply.github.com>
2022-05-08 22:41:10 -0400

    Add files via upload

---
+++ Python_practice.py
@@ -0,0 +1,83 @@
+temperature = int(input("What is the temperature outside? "))
+if temperature > 80:
+    print("Turn on the AC.")
+else:
+    print("Open the windows.")
+    # What is the score?
+score = int(input("What is your test score? "))
+
+# Determine the grade.
+if score >= 90:
+    print('Your grade is an A.')
+elif score >= 80:
+    print('Your grade is a B.')
+elif score >= 70:
+    print('Your grade is a C.')
+elif score >= 60:
+    print('Your grade is a D.')
+else:
+    print('Your grade is an F.')
+#counties 
+counties = ["Arapahoe","Denver","Jefferson"]
+if "El Paso" in counties:
+    print("El Paso is in the list of counties.")
+else:
+    print("El Paso is not the list of counties.")
+#loops 
+    for county in counties:
+    print(county)
+
+    #NUM 
+    numbers = [0, 1, 2, 3, 4]
+for num in numbers:
+    print(num)
+
+for i in range(len(counties)):
+    print(counties[i])
+
+counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
+for county in counties_dict:
+    print(county)
+for county in counties_dict.keys():
+    print(county)
+for voters in counties_dict.values():
+    print(voters)
+for county in counties_dict:
+    print(counties_dict.get(county))
+for county, voters in counties_dict.items():
+    print(county, voters)
+    #Voting Data 
+
+voting_data = [{"county":"Arapahoe", "registered_voters": 422829},
+{"county":"Denver", "registered_voters": 463353},
+{"county":"Jefferson", "registered_voters": 432438}]
+for county_dict in voting_data:
+    print(county_dict)
+
+for county_dict in voting_data:
+    for value in county_dict.values():
+        print(value)
+for county_dict in voting_data:
+
+     print(county_dict['registered_voters'])
+for county_dict in voting_data:
+    print(county_dict['county'])
+    #Editing
+
+my_votes = int(input("How many votes did you get in the election? "))
+total_votes = int(input("What is the total votes in the election? "))
+print(f"I received {my_votes / total_votes * 100}% of the total votes.")
+#
+# f-string
+counties_dict = {"Arapahoe": 369237, "Denver":413229, "Jefferson": 390222}
+for county, voters in counties_dict.items():
+    print(f"{county} county has {voters} registered voters.")
+# F-string to put everything in one line 
+candidate_votes = int(input("How many votes did the candidate get in the election? "))
+total_votes = int(input("What is the total number of votes in the election? "))
+message_to_candidate = (
+    f"You received {candidate_votes} number of votes. "
+    f"The total number of votes in the election was {total_votes}. "
+    f"You received {candidate_votes / total_votes * 100}% of the total votes.")
+
+print(message_to_candidate)
